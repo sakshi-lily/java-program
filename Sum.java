b49c8f990f0f1ade4c4cc5bbffc8e8ca2a81c6a8
Sakshi <sakshidk1502@gmail.com>
2025-08-06 08:34:34 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,252 @@
+/*package core.com;
+import java.util.*;
+public class Sum {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 3;
+		int sum =  num1 +  num2 ;
+		System.out.println("SUM="+sum);	
+		
+		
+	}
+
+}
+*/
+
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int sum=a+b-c;
+        System.out.println("Answer="+answer);
+    }
+    
+}*/
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int length=sc.nextInt();
+        int breath=sc.nextInt();
+        
+        int area = length* breath;
+        System.out.println("Area="+area);
+    }
+    
+}*/
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int radius=sc.nextInt();
+        float area=3.14f+(radius*radius);
+        
+        System.out.println("Area="+area);
+    }
+    
+}*/
+
+
+
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int princple=sc.nextInt();
+        int rate=sc.nextInt();
+        int time=sc.nextInt();
+        int si=(princple*rate*time)/100; 
+        
+        
+        System.out.println("Simple interest="+si);
+    }
+    
+}
+*/
+
+//Ek single character ko print karana kesi bhi word m s 
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char s = sc.next().charAt(0);
+        System.out.println(s);
+    }
+    
+}*/
+
+//Typecasting 
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a = 130;
+        byte b = (byte)a;
+      
+        System.out.println(b);
+    }
+    
+}
+*/
+//1.Java Program to Read The Number From Standard Input 
+
+
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);  
+        System.out.print("Number: ");
+        int num = sc.nextInt();               
+        System.out.println("Weight " + num);
+        
+    }
+    
+}*/
+
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);  
+    	int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " ");
+        }
+
+        
+    }
+    
+}*/
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);  
+        System.out.println(n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+            System.out.print(i + " ");
+        }
+
+        
+    }
+    
+}*/
+
+
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);  
+        System.out.println(n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+            System.out.print(i + " ");
+        }
+
+        
+    }
+    
+}
+*/
+
+
+
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);  
+        System.out.println("Number");
+        int n =sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum+=i;
+        }
+
+            System.out.println("sum = " + sum);
+        
+
+        
+    }
+    
+}*/
+
+/*package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);  
+        System.out.println("Number");
+        int n =sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+            System.out.println("Factorial = " + fact);
+        
+
+        
+    }
+    
+}*/
+
+
+package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+    	Scanner sc = new Scanner(System.in);  
+        System.out.println("Number");
+        int n =sc.nextInt();
+        int  = 1;
+        for (int i = 1; i <= 10; i++) {
+            fact = fact * i;
+        }
+
+            System.out.println("Factorial = " + fact);
+        
+
+        
+    }
+    
+}
+
+
+
+
+
+
+
