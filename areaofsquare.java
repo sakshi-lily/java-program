5e014a9da455c221dbf36ccc7ddd3712754df6ce
Sakshi <sakshidk1502@gmail.com>
2025-09-15 00:22:33 +0530

    Create areaofsquare.java

---
+++ areaofsquare.java
@@ -0,0 +1,13 @@
+package core.com;
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int length=sc.nextInt();
+        int breath=sc.nextInt();
+        
+        int area = length* breath;
+        System.out.println("Area="+area);
+    }
+    
+}
