553d46427635f00232a1b71541549d11e59e4b67
Sakshi <sakshidk1502@gmail.com>
2025-09-16 07:51:54 +0530

    Create areaofcircle.java

---
+++ areaofcircle.java
@@ -0,0 +1,10 @@
+2public class Sum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int radius=sc.nextInt();
+        float area=3.14f+(radius*radius);
+        
+        System.out.println("Area="+area);
+    }
+    
+}
